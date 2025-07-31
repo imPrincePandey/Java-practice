98dad56cc5ec9901850269fb3d1ebc72689c8f07
Prince Kumar <imprincepandey1912@gmail.com>
2025-08-01 00:27:17 +0530

    create method for calculate factorial

---
+++ src/method/Practice5.java
@@ -0,0 +1,12 @@
+package method;
+
+public class Practice5 {
+	
+	static void greetUser(String name) {
+		System.out.println("My name is " + name + "Welcome to java!");
+	}
+	public static void main(String[] args) {
+		greetUser("Prince Pandey ");
+	}
+
+}
