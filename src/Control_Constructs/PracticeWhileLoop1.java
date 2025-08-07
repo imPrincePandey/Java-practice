938ec44da69f65ee14a73e7c67a8ce997ad2bfae
Prince Kumar <imprincepandey1912@gmail.com>
2025-08-07 19:49:20 +0530

    Adding Some While Loop Problem today

---
+++ src/Control_Constructs/PracticeWhileLoop1.java
@@ -0,0 +1,15 @@
+package Control_Constructs;
+
+public class PracticeWhileLoop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=1;
+		while(number<=10) {
+			System.out.println(number);
+			number++;
+		}
+
+	}
+
+}
