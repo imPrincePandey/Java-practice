4b9a41099a40f6ca8bab847ba8903f1285a68a66
Prince Kumar <imprincepandey1912@gmail.com>
2025-08-19 16:34:53 +0530

    Adding star pattern with number problem

---
+++ src/Loop/Pattern9.java
@@ -0,0 +1,27 @@
+package Loop;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j= 1; j <= 5; j++) {
+				System.out.print("*");
+			}
+			
+			// loop for next part
+			
+			for (int k = 5; k >= i; k--) {
+				System.out.print(i);
+			}
+			
+			
+			
+			System.out.println();
+		}
+
+
+
+	}
+
+}
